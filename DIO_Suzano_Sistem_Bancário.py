d61917ca54259617951cb8279fe3f6e31eeceba8
Henrique <henriquehlx@gmail.com>
2025-09-16 14:47:01 -0300

    Desafio 2 - Inclusão datas e limite de 10 transações

--- DIO_Suzano_Sistem_Bancário.py
+++ DIO_Suzano_Sistem_Bancário.py
@@ -1,5 +1,10 @@
 # Sistema Bancário Simples
 # Autor - Henrique Leite
+#Última alteração: Incluído data e hora e limite de 10 transações
+
+from datetime import datetime
+
+
 
 # VARIÁVEIS
 saldo =0
@@ -7,6 +12,7 @@ limite = 500
 # extrato=""
 numero_saque=0
 LIMITE_SAQUES = 3
+LIMITE_TRANSACOES = 10
 
 #Depósito
 ''' - Depositar apenas valores positivos e inteiros;
@@ -14,23 +20,41 @@ LIMITE_SAQUES = 3
     - Depósitos devem ser armazenados em uma variável e exibida em extrato;
     - '''
 depositos = []
+transacoes = []
 saques=[]
+
+
+def contador_transacoes_hoje():
+   #Contar quantas transações foram feitas hoje
+    hoje = datetime.now().date()
+    return sum(1 for t in transacoes if t["data"].date() == hoje)
 
 def depositar(valor):
     print('---------- OPERAÇÃO DE DEPÓSITO ----------')
     global saldo 
-   
+
+    if contador_transacoes_hoje()>= LIMITE_TRANSACOES:
+       print ('Limite de 10 transações diárias atingido. Operação não permitida.')
+       return
     
     if valor>0:
         print (f"Depósito realizado com Sucesso - Valor R$ {valor:.2f}")
         depositos.append(f'Depósito = R$ {valor:.2f}')
         saldo+=valor
+        transacoes.append({
+           "tipo": "Depósito",
+           "valor": valor,
+           "data": datetime.now()
+        })      
+
         print (f"-- Saldo Atualizado Após Depósito: R$ {saldo}")
 
     else:
         print ("Você não digitou um número inteiro, refaça a operação")
 
     return (valor)
+
+
  #Saque 
 
 ''' - 03 Saques diários
@@ -38,15 +62,28 @@ def depositar(valor):
     - Se não tiver saldo: Não será possível sacar o dinheiro por falta de saldo
     - Todos os saques devem ser armazenados em uma variável e exibidas em extrato
     - '''
+
+
 def sacar(valor):
     print('---------- OPERAÇÃO DE SAQUE ----------')
     global saldo 
-        
+    
+    if contador_transacoes_hoje()>= LIMITE_TRANSACOES:
+       print ('Limite de 10 transações diárias atingido. Operação não permitida.')
+       return
+
+
     if saldo > 0: 
         print (f"Saque realizado com Sucesso - Valor R$ {valor:.2f}")
         saldo-=valor
         saques.append(f'Saque = R$ {valor:.2f}')
         print (f"-- Saldo Atualizado Após Saque: R$ {saldo}")
+        transacoes.append({
+           "tipo": "Saque",
+           "valor": valor,
+           "data": datetime.now()
+        })    
+
 
     else:
         print(f"Saldo insuficiente para saque: Saldo Atual = R$ {saldo:.2f}")
@@ -61,11 +98,15 @@ def sacar(valor):
 
 def extrato():
     print('---------- EXTRATO BANCÁRIO ----------')
-    for i in depositos:
-     print(i)
-    for s in saques:
-     print (s)
-    print(f"SALDO ATUAL: R$ {saldo}")
+    if not transacoes:
+       print ("Nenhuma transação realizada")
+    else:
+           
+        for t in transacoes:
+            data_formatada = t["data"].strftime('%d/%m/%Y %H:%M')
+            print (f'{t['tipo']} = R$ {t['valor']:.2f} | {data_formatada}')
+    print (f'Saldo Atual: R$ {saldo:.2f}')
+    print (f'Número transaçoes hoje: {contador_transacoes_hoje()}/{LIMITE_TRANSACOES}')
    
 menu = '''
 
